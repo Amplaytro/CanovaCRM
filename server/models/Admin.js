2b3f470a774a2a4072714e5100eecdba550a6eeb
Amplaytro <48793390+Amplaytro@users.noreply.github.com>
2026-04-28 23:31:05 +0530

    feat: create Admin model schema extending the base account schema

--- server/models/Admin.js
+++ server/models/Admin.js
@@ -3,6 +3,4 @@ const accountSchema = require('./accountSchema');
 
 const adminSchema = accountSchema.clone();
 
-adminSchema.index({ email: 1 }, { unique: true });
-
 module.exports = mongoose.models.Admin || mongoose.model('Admin', adminSchema, 'admin');
